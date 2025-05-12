f18b7d31b21dbf8076101fedf96e24f6f8c56cff
Michał Moczulski <michalmoczulski1a@gmail.com>
2025-05-12 18:13:04 +0200

    Add support for NonNilUuid (#1658)

--- src/types/external/uuid.rs
+++ src/types/external/uuid.rs
@@ -1,5 +1,4 @@
-use uuid::Uuid;
-
+use uuid::{NonNilUuid, Uuid};
 use crate::{InputValueError, InputValueResult, Scalar, ScalarType, Value};
 
 #[Scalar(
@@ -14,11 +13,40 @@ use crate::{InputValueError, InputValueResult, Scalar, ScalarType, Value};
 /// # References
 ///
 /// * [Wikipedia: Universally Unique Identifier](http://en.wikipedia.org/wiki/Universally_unique_identifier)
-/// * [RFC4122: A Universally Unique IDentifier (UUID) URN Namespace](http://tools.ietf.org/html/rfc4122)
+/// * [RFC4122: A Universally Unique Identifier (UUID) URN Namespace](http://tools.ietf.org/html/rfc4122)
 impl ScalarType for Uuid {
     fn parse(value: Value) -> InputValueResult<Self> {
         match value {
             Value::String(s) => Ok(Uuid::parse_str(&s)?),
+            _ => Err(InputValueError::expected_type(value)),
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::String(self.to_string())
+    }
+}
+
+#[Scalar(
+    internal,
+    name = "NonNilUUID",
+    specified_by_url = "http://tools.ietf.org/html/rfc4122"
+)]
+/// A UUID is a unique 128-bit number, stored as 16 octets. UUIDs are parsed as
+/// Strings within GraphQL. UUIDs are used to assign unique identifiers to
+/// entities without requiring a central allocating authority.
+///
+/// # References
+///
+/// * [Wikipedia: Universally Unique Identifier](http://en.wikipedia.org/wiki/Universally_unique_identifier)
+/// * [RFC4122: A Universally Unique Identifier (UUID) URN Namespace](http://tools.ietf.org/html/rfc4122)
+impl ScalarType for NonNilUuid {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::String(s) => {
+                let uuid = Uuid::parse_str(&s)?;
+                Ok(NonNilUuid::try_from(uuid)?)
+            },
             _ => Err(InputValueError::expected_type(value)),
         }
     }
--- tests/serializer_uuid.rs
+++ tests/serializer_uuid.rs
@@ -13,9 +13,11 @@ async_graphql::scalar!(AssetId);
 /// https://github.com/async-graphql/async-graphql/issues/603
 pub async fn test_serialize_uuid() {
     let generated_uuid = uuid::Uuid::new_v4();
+    let generated_non_nil_uuid = uuid::NonNilUuid::new(generated_uuid).unwrap();
 
     struct Query {
         data: AssetId,
+        non_nil: uuid::NonNilUuid
     }
 
     #[Object]
@@ -23,20 +25,25 @@ pub async fn test_serialize_uuid() {
         async fn data(&self) -> &AssetId {
             &self.data
         }
+        async fn non_nil(&self) -> &uuid::NonNilUuid {
+            &self.non_nil
+        }
     }
 
     let schema = Schema::new(
         Query {
             data: AssetId(generated_uuid),
+            non_nil: generated_non_nil_uuid,
         },
         EmptyMutation,
         EmptySubscription,
     );
-    let query = r#"{ data }"#;
+    let query = r#"{ data, non_nil }"#;
     assert_eq!(
         schema.execute(query).await.data,
         value!({
             "data": generated_uuid.to_string(),
+            "non_nil": generated_non_nil_uuid.to_string(),
         })
     );
 }
